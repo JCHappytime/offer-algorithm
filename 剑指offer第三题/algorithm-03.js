8d893e15827d4eb50397ed048b26abeeeeb81a2b
JCHappytime <18782959613@163.com>
2020-09-29 19:49:59 +0800

    增加第三题的解法2与分析

--- 剑指offer第三题/algorithm-03.js
+++ 剑指offer第三题/algorithm-03.js
@@ -11,3 +11,32 @@ function fabonacci(n) {
   }
   return this.fabonacci(n-1) + this.fabonacci(n-2);
   }
+
+
+// <<<<<===================  解释  ===================>>>>>
+ /**
+    递归解法也比较好理解，但是递归方式非常耗时，会导致内存和时间都超出限制。
+ */
+
+ // 解法一
+function fabonacci(n) {
+  let arr = [1, 1];
+  if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    for (let i = 2; i < n; i ++ ) {
+      arr.push[arr[n - 1] + arr[n - 2]];
+    }
+  }
+  return arr[n - 1];
+}
+
+
+// <<<<<===================  解释  ===================>>>>>
+ /**
+    增加非递归解法的分析：
+    1.当n=1与n=2时，f(n) =1;
+    所以设置arr = [1, 1];
+    2. 当n >2时循环遍历，将值push到数组arr中
+    arr.push(arr[n-1] + arr[n-2]);
+ */
